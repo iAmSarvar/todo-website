45240ea4bc1e01cd2cd873ed0bd3eb6fcb1ac1b7
iAmSarvar <iamsarvarsultanov@gmail.com>
2024-06-05 00:05:47 +0500

    Result component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
+import Result from "./components/Result";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="container">
+      <Result />
+    </div>
+  );
 };
 export default App;
---
+++ src/components/Result.jsx
@@ -0,0 +1,14 @@
+const Result = () => {
+  return (
+    <div className="result">
+      <div>
+        <h1>Todo done</h1>
+        <h3>Keep it up</h3>
+      </div>
+      <div className="result-score">
+        <h3>1/3</h3>
+      </div>
+    </div>
+  );
+};
+export default Result;
